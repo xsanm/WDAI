4f45d72183bd7af07c3d82e38287d4be4e6dc01e
xsan <kkurowski.kk@gmail.com>
2021-01-10 17:09:36 +0100

    adding register form, without validators

--- lab6/ex1/src/app/register/register.component.ts
+++ lab6/ex1/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
+  hide = true;
 
   ngOnInit(): void {
   }
 
+  email = new FormControl('', [Validators.required, Validators.email]);
+  pass1 = new FormControl('', [Validators.required]);
+  pass2 = new FormControl('', [Validators.required]);
+  userName = new FormControl('', [Validators.required]);
+  firstName = new FormControl('', [Validators.required]);
+  lastName = new FormControl('', [Validators.required]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+    if (this.pass1.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  register() {
+    console.log("sign in");
+    console.log(this.email.value);
+    console.log(this.pass1.value);
+  }
+
+  checkValidData() {
+    return this.email.valid && this.pass1.value && this.pass2.valid &&
+      this.userName.valid && this.firstName.valid && this.lastName.valid;
+  }
+
+  login() {
+    const dialogRef = this.dialog.open(LoginComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+
+  }
+
 }
+
